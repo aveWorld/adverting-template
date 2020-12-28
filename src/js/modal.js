39def720db033124359b274e83160afe7ca978c5
aveWorld <texnarb228@gmail.com>
2020-12-28 12:42:08 +0200

    fix scroll issue

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,14 @@
 const headerBtn = document.querySelector('.header__btn');
 const modal = document.querySelector('.modal');
 const exitModal = document.querySelector('.modal__exit');
+const body = document.querySelector('body');
 
 headerBtn.addEventListener('click', () => {
   modal.style.display = 'flex';
+  body.style.overflow = 'hidden';
 });
 
 exitModal.addEventListener('click', () => {
   modal.style.display = 'none';
+  body.style.overflow = 'auto';
 });
