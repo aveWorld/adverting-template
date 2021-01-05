8feb003807bff9d2d09be0dfd5465e6b509e325e
aveWorld <texnarb228@gmail.com>
2021-01-05 15:39:46 +0200

    add sign up modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,27 @@
 const headerBtn = document.querySelector('.header__btn');
 const modal = document.querySelector('.modal');
 const exitModal = document.querySelector('.modal__exit');
+const account = document.getElementById('account');
+const modalAcc = document.querySelector('.modal__account');
+const exitModalAcc = document.querySelector('.modal__exit__account');
 const body = document.querySelector('body');
 
 headerBtn.addEventListener('click', () => {
   modal.style.display = 'flex';
   body.style.overflow = 'hidden';
+});
+
+account.addEventListener('click', () => {
+  // close current modal
+  modal.style.display = 'none';
+  body.style.overflow = 'auto';
+
+  modalAcc.style.display = 'flex';
+  body.style.overflow = 'hidden';
+  exitModalAcc.addEventListener('click', () => {
+    modalAcc.style.display = 'none';
+    body.style.overflow = 'auto';
+  });
 });
 
 exitModal.addEventListener('click', () => {
